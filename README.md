581451ff46cbde53384ccf43ac8142776386a603
fahmisf19 <fahmisfatalnuck@gmail.com>
2023-10-01 21:36:26 +0700

    feat: add silogistik app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tugas Individu APAP
+
+## Authors
+
+* **Fahmi Sabila Firdaus** - *2106751745* - *B* 
---
+++ silogistik2106751745/src/main/java/apap/ti/silogistik2106751745/Silogistik2106751745Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106751745;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106751745Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106751745Application.class, args);
+	}
+
+}
